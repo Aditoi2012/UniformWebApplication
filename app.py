1f3c6c866d3a2d57958598038e51cfd2e015a891
16327 <16327@wc.school.nz>
2020-08-07 10:29:42 +1200

    Recent project

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 app.run(host='0.0.0.0', debug=True)
